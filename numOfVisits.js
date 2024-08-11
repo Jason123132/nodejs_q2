128c1020d95d23fdb9b383d3290268f4fcabcfaf
Jason123132 <167475616+Jason123132@users.noreply.github.com>
2024-08-10 22:37:11 -0400

    Add files via upload

---
+++ numOfVisits.js
@@ -0,0 +1,31 @@
+const express=require('express');
+const cookieParser=require('cookie-parser');
+const app=express();
+const port=8001;
+
+app.use(cookieParser());
+
+app.get('/',(req,res)=>{
+	let visitCount=req.cookie.visitCount;
+	let lastVisit=req.cookie.lastVisit;
+	
+	console.log('Cookies:', req.cookies);
+	
+	if(visitCount){
+		visitCount=parseInt(visitCount)+1;
+		const now=new Date();
+		res.cookie('visitCount',visitCount);
+		res.cookie('lastVisit',now.toString());
+		
+		res.send(`Hello, this is the ${visitCount} time that you are visiting my webpage. <br>
+          Last time you visited my webpage on: ${lastVisit}`);
+	}else{
+		res.cookie('visitCount',1);
+		res.cookie('lastVisit',new Date().toString());
+		res.send('Welcome to my webpage! It is your first time that you are here.'); 
+	}
+});
+
+app.listen(port,()=>{
+	console.log(`Server running at http://localhost:${port}`);
+});
